9170726e6a38fc8a0f1e9323f86b07556ed85532
Gavin Wu <adolf.bill.gavin@gmail.com>
2019-03-20 11:40:45 +0800

    添加获取本机IP主机名方法

--- util_tools.py
+++ util_tools.py
@@ -1,6 +1,8 @@
 from logbook import Logger, StreamHandler, TimedRotatingFileHandler
 from logbook.more import ColorizedStderrHandler
 import logbook
+import socket
+import re
 import sys
 import fire
 import os
@@ -15,9 +17,6 @@ def logger(name='LOGBOOK', log_path='', file_log=False):
 	if file_log:
 		TimedRotatingFileHandler(os.path.join(log_dir, '%s.log' % name.lower()), date_format='%Y-%m-%d', bubble=True).push_application()
 	return Logger(name)
-
-def abspath(path):
-	return os.path.join(os.path.dirname(__file__), path)
 
 def bytes2human(n):
 	symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
@@ -40,6 +39,49 @@ def filesize(path):
 				continue
 			total_size += os.path.getsize(fpath)
 	return bytes2human(total_size)
+
+
+class switch(object):
+
+
+    def __init__(self, value, flag=0):
+        '''
+        re.S DOTALL
+        re.I IGNORECASE
+        re.L LOCALE
+        re.M MULTILINE
+        re.X VERBOSE
+        re.U
+        '''
+        self.value = value
+        self.fall = False
+        self.flag = flag
+
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+
+    def match(self, arg=''):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not arg:
+            return True
+        elif re.search(arg, self.value, self.flag) is not None:
+            self.fall = True
+            return True
+        else:
+            return False
+
+
+def get_ip_hostname(ip='8.8.8.8', port=80):
+    h = socket.gethostname()
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((ip, port))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return h, ip
 
 if __name__ == '__main__':
 	fire.Fire()
