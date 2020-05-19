a0318edf05dc0e9fbd518e3360e7f198bfec3371
Gavin Wu <471456858@163.com>
2020-05-19 19:53:36 +0800

    地精排序

--- sleep_sort.py
+++ sleep_sort.py
@@ -24,25 +24,37 @@ def bogoSort(*a):
 
 
 def sleepPut(num):
-    print(f'Thread sleep {num}ms')
-    time.sleep(num / 1000)
+    time.sleep(num / 100)
     queue.put(num)
 
 def sleepSort(*a):
     start = time.time() * 1000
     global queue
     queue = queue.Queue()
-    lvars = list(a)
+    mina = min(a)
+    lvars = []
+    for i in a:
+        lvars.append(i - mina)
     pool = threadpool.ThreadPool(len(lvars))
     requests = threadpool.makeRequests(sleepPut, lvars)
     [pool.putRequest(req) for req in requests]
     pool.wait()
     res = []
     while not queue.empty():
-        res.append(queue.get())
+        res.append(queue.get() + mina)
     print(f'running {time.time() * 1000 - start}ms')
     return res
 
+def gnomeSort(*a):
+    la = list(a)
+    index = 1
+    while index < len(la):
+        if index > 0 and la[index] < la[index - 1]:
+            la[index], la[index - 1] = la[index - 1], la[index]
+            index -= 1
+        else:
+            index += 1
+    return la
 
 if __name__ == '__main__':
     fire.Fire()
